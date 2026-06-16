5ccc4b82db4534f8944a297f344bf73d6ff4a881
priyanshi yadav <priyanshi.yadav2025@glbajajgroup.org>
2026-06-16 14:38:46 +0530

    Day2 Assignment Uploaded

---
+++ day1/day2/checkwhethernumberispalindrome.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int main() {
+    int num, original, rebverse = 0, remainder;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    original = num;
+    while (num != 0) {
+        remainder = num % 10;
+        rebverse = rebverse * 10 + remainder;
+        num /= 10;
+    }
+    if (original == rebverse) {
+        printf("The number is a palindrome.\n");
+    } else {
+        printf("The number is not a palindrome.\n");
+    }
+    return 0;
+}
---
+++ day1/day2/findsumofdigitsofanumber.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        sum += n % 10; // Add the last digit to sum
+        n /= 10; // Remove the last digit
+    }
+    printf("The sum of the digits in the given number is: %d\n", sum);
+    return 0;
+}
