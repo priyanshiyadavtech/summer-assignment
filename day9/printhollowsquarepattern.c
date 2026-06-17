4b300a1097b1592fa2687c355c3dbfdb3ccf8633
priyanshi yadav <priyanshi.yadav2025@glbajajgroup.org>
2026-06-17 11:59:22 +0530

    day9 assignment uploaded

---
+++ day9/printhollowsquarepattern.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int rows;
+    printf("Enter the number of rows for the hollow square pattern: ");
+    scanf("%d", &rows);
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows; j++) {
+            if (i == 1 || i == rows || j == 1 || j == rows) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
