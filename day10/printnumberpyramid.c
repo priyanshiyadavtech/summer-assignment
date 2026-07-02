95d48e996af729c90391f5ab9b534f6892e1d550
priyanshi yadav <priyanshi.yadav2025@glbajajgroup.org>
2026-07-02 15:40:43 +0530

    day10 assignment uploaded

--- day10/printnumberpyramid.c
+++ day10/printnumberpyramid.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
-
 int main() {
-    int n, i, j, k, n=5;
+    int n, i, j, k;
+    n = 5;
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= n - i; j++) {
             printf(" ");
